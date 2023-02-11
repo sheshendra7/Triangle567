8a627a0046220ced1f8cba4f392424d5a1a72c18
sheshendra7 <sheshendra7@gmailc.om>
2023-02-10 20:04:03 -0500

    trianglefile commit

--- Triangle.py
+++ Triangle.py
@@ -28,30 +28,23 @@ def classifyTriangle(a,b,c):
     """
 
     # require that the input values be >= 0 and <= 200
-    if a > 200 or b > 200 or c > 200:
+    if not (isinstance(a, int) and isinstance(b, int) and isinstance(c, int)):
         return 'InvalidInput'
-        
-    if a <= 0 or b <= b or c <= 0:
+
+    if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
-    
-    # verify that all 3 inputs are integers  
-    # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
-      
-    # This information was not in the requirements spec but 
-    # is important for correctness
-    # the sum of any two sides must be strictly less than the third side
-    # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+
+    # Check for Triangle Property : The sum of the length of the two sides
+    # of a triangle is greater than the length of the third side.
+    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
         return 'NotATriangle'
-        
-    # now we know that we have a valid triangle 
-    if a == b and b == a:
+
+    # After All checks above, We can Classify the Triangles
+    if a == b and b == c:
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
+    elif ((a ** 2) + (b ** 2)) == (c ** 2) or ((a ** 2) + (c ** 2)) == (b ** 2) or ((b ** 2) + (c ** 2)) == (a ** 2):
         return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
+    elif (a != b) and (b != c) and (a != c):
         return 'Scalene'
     else:
-        return 'Isoceles'
+        return 'Isosceles'
